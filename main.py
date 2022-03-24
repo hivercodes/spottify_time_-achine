880bebb18aec0f5d648a42a03460df87aa69e24c
Lars <lars.eriksson.mail@gmail.com>
2022-03-24 19:20:58 +0100

    Got auth working with spotify. man that documentation is optuse.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 #date = input("Please enter a time in the YYYY-MM-DD format:")
 
@@ -18,5 +19,31 @@ for l in data_list:
     song_list.append(l.text.replace("\t", "").replace("\n", ""))
 
 
-print(song_list)
 
+auth_list = []
+
+with open("../api/spotify") as auth:
+    base_auth = auth.readlines()
+    for inf in base_auth:
+        auth_list.append(inf.strip("\n"))
+
+
+oauth = {
+    "client_id": auth_list[0],
+    "client_secret": auth_list[1],
+    "redirect_url": auth_list[2],
+    "scope": "playlist-modify-private playlist-modify-public"
+}
+
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri=auth_list[2],
+        client_id=auth_list[0],
+        client_secret=auth_list[1],
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
